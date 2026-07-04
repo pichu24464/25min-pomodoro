ed946001de2e63c701513ea6ae7c64a1b37b4359
newbiedeveloper968 <newbiedeveloper968@gmail.com>
2026-07-05 01:56:15 +0600

    updated dirs

--- js/welcome.js
+++ js/welcome.js
@@ -1,14 +1,14 @@
 const username = document.getElementById("name").value;
 function sendName() {
+    const username = document.getElementById("name").value.trim();
+
     localStorage.setItem("username", username);
     window.location.href = "index.html";
 }
 
 var input = document.getElementById('name');
 input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    localStorage.setItem("username", username);
-    document.getElementById("continueBtn").click();
-  }
+    if (event.key === "Enter") {
+        sendName();
+    }
 });
